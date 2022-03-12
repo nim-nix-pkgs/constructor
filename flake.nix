a5a3c96586c18729957a100dafa201d399068f03
hugosenari <hugosenari@gmail.com>
2022-03-12 04:12:11 -0300

    chore: re index constructor

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim macros to aid in object construction including event programming, and constructors.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."constructor-master".url = "path:./master";
+  inputs."constructor-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."constructor-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
