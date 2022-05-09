78dcad61f3494551a01ab96897733f481fd847c9
hugosenari <hugosenari@gmail.com>
2022-05-09 20:36:17 -0300

    chore: re index constructor

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."constructor-master".dir   = "master";
-  inputs."constructor-master".owner = "nim-nix-pkgs";
-  inputs."constructor-master".ref   = "master";
-  inputs."constructor-master".repo  = "constructor";
-  inputs."constructor-master".type  = "github";
-  inputs."constructor-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."constructor-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
